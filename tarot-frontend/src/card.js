831ca4f87dfec03145e9b8645133b1b432274b16
ronniekram <arynn.boniface@gmail.com>
2020-07-11 16:24:37 -0700

    added js classes and constructors for card and draw

---
+++ tarot-frontend/src/card.js
@@ -0,0 +1,10 @@
+class Card {
+  constructor(cardDetails) {
+    this.name = cardDetails.name
+    this.summary = cardDetails.summary
+    this.full_meaning = cardDetails.full_meaning
+    this.image = cardDetails.image
+    this.upright = cardDetails.upright
+    this.reversed = cardDetails.reversed
+  }
+}
---
+++ tarot-frontend/src/draw.js
@@ -0,0 +1,6 @@
+class Draw {
+  constructor(drawDetails) {
+    this.num_of_cards = drawDetails.num_of_cards
+    this.created_at = drawDetails.created_at
+  }
+}
