1dd07552e3a63af9c34c9e75e06e7e4761c43404
ronniekram <arynn.boniface@gmail.com>
2020-07-11 19:14:08 -0700

    cards index loading and creating cards

--- tarot-frontend/src/index.js
+++ tarot-frontend/src/index.js
@@ -1,9 +1,9 @@
 const BASE_URL = "http://localhost:3000"
 const navBar = document.querySelector("nav")
-const rowDiv = document.getElementById("main-box")
+const rowDiv = document.querySelector(".row")
 
 document.addEventListener("DOMContentLoaded", () => {
-  Card.fetchCards();
+  fetchCards();
   // event listener for nav bar
   // event listener for one card draw
   // event listener for three card draw
@@ -13,4 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
 function navigateTarot() {
   // use event target for button ids
   // use event targets for nav links
+}
+
+function fetchCards() {
+  fetch(`${BASE_URL}/cards`)
+  .then(resp => resp.json())
+  .then(cards => this.renderCards(cards))
+}
+
+function renderCards(cards) {
+  cards.forEach(card => {
+    let id = card.id
+    let columnDiv = document.createElement('div');
+    columnDiv.className = "col-md-4 mb-5"
+    columnDiv.id = id
+
+    columnDiv.innerHTML += `
+      <div class="card h-100">
+      <div class="card-body" id="${id}">
+        <h2 class="card-title" id="${id}"> ${card.name}</h2>
+        <div class="card-img" id="${id}">
+          <img src="${card.image}" class="card-img">
+        </div>
+        <p class="card-text" id="${id}">${card.full_meaning}</p>
+      </div>
+      <div class="card-footer" id="${id}">
+        <p>
+          <strong>Upright:</strong> ${card.upright} 
+        </p>
+        <p>
+          <strong>Reversed:</strong> ${card.reversed}
+        </p>
+      </div>
+    </div>
+    `
+    rowDiv.appendChild(columnDiv)
+  })
 }
