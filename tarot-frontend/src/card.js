8238151a1013be0414bb0149809b886b18894e84
ronniekram <arynn.boniface@gmail.com>
2020-07-18 15:49:05 -0700

    cards are listed with suite name next to title, need to separate into cards

--- tarot-frontend/src/card.js
+++ tarot-frontend/src/card.js
@@ -18,18 +18,28 @@ class Card{
         // Card.renderCards(cards);
       });
   }
-    static renderCards(cards) {
-      let ol = document.createElement('ol');
-      cards.forEach(card => {
-        let name = titleCaseName(card.name);
-        let li = document.createElement('li');
-        li.innerHTML = `
-        <strong>${name}</strong> - ${card.summary}
-        `
-        ol.appendChild(li);
-      });
-      rowDiv.appendChild(ol);
-    }
+    // static renderCards(cards) {
+    //   cards.forEach(card => {
+    //     let name = titleCaseName(card.name);
+    //     let li = document.createElement('li');
+    //     li.innerHTML = `
+    //     <strong>${name}</strong> - ${card.summary}
+    //     `
+    //     ol.appendChild(li);
+    //   });
+    //   rowDiv.appendChild(ol);
+    // }
+
+    // static renderCard(card, suite) {
+    //   let ol = document.createElement('ol');
+    //   ol.className = "suites-list";
+    //   let name = titleCaseName(card.name);
+    //   let li = document.createElement('li');
+    //   li.innerHTML = `
+    //   <strong>${suite}</strong>: <strong>${name}</strong> - ${card.summary}
+    //   `
+    //   ol.appendChild(li);
+    // }
 
     static splitCards(cards) {
       let major = cards.splice(0,22);
@@ -37,11 +47,56 @@ class Card{
       let cups = cards.splice(0,14);
       let wands = cards.splice(0,14);
       let pentacles = cards.splice(0,14)
-      console.log(major);
-      console.log(swords);
-      console.log(cups);
-      console.log(wands);
-      console.log(pentacles);
+      let ol = document.createElement('ol');
+
+      major.forEach(card => {
+        let name = titleCaseName(card.name);
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <strong>Major Arcana</strong>: <strong>${name}</strong> - ${card.summary}
+        `
+        ol.appendChild(li);
+      });
+
+      swords.forEach(card => {
+        let name = titleCaseName(card.name);
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <strong>Swords</strong>: <strong>${name}</strong> - ${card.summary}
+        `
+        ol.appendChild(li);
+      });
+
+      cups.forEach(card => {
+        let name = titleCaseName(card.name);
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <strong>Cups</strong>: <strong>${name}</strong> - ${card.summary}
+        `
+        ol.appendChild(li);
+      });
+
+      wands.forEach(card => {
+        let name = titleCaseName(card.name);
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <strong>Wands:</strong>: <strong>${name}</strong> - ${card.summary}
+        `
+        ol.appendChild(li);
+      });
+
+      pentacles.forEach(card => {
+        let name = titleCaseName(card.name);
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <strong>Pentacles</strong>: <strong>${name}</strong> - ${card.summary}
+        `
+        ol.appendChild(li);
+      });
+
+      rowDiv.appendChild(ol);
+
+
     }
       
     }
